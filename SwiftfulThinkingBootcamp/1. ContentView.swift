b46f4f23a33935702555fbaea2bd792c5efdf195
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-10-04 11:13:13 +0900

    Migrates to SwiftUI and adds bootcamp lessons
    Refactors the project to use SwiftUI, replacing the initial SwiftData setup.
    
    Adds several new files, each representing a lesson in a SwiftUI bootcamp, covering various SwiftUI concepts and components.
    
    Removes initial content view files and test files.
    Adds new assets for adaptive colors, images and vector graphics

---
+++ SwiftfulThinkingBootcamp/1. ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Brandon Jones on 8/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+       Text("Hello, world!")
+        .padding()
+    }
+
+
+}
+
+#Preview {
+    ContentView()
+}
