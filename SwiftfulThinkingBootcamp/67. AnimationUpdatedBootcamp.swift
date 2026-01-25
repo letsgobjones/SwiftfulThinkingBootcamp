1a9a8e363e1c9af80918a954aa08f6536ae30e42
BrandonSoulstice <203486775+BrandonSoulstice@users.noreply.github.com>
2026-01-25 19:05:22 +0900

    feat(animations): explore spring animations
    Adds AnimationUpdatedBootcamp (Topic #67) with spring parameters.
    - Compares different spring durations and bounce values.
    - Updates animation logic for smoother transitions.

---
+++ SwiftfulThinkingBootcamp/67. AnimationUpdatedBootcamp.swift
@@ -0,0 +1,45 @@
+//
+//  AnimationUpdatedBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Brandon Jones on 1/18/26.
+//
+
+import SwiftUI
+
+struct AnimationUpdatedBootcamp: View {
+  @State private var animate1: Bool = false
+  @State private var animate2: Bool = false
+  var body: some View {
+    ZStack {
+      VStack(spacing: 40) {
+        Button("Action 1") {
+          animate1.toggle()
+        }
+        
+        Button("Action 2") {
+          animate2.toggle()
+        }
+        
+        ZStack {
+          Rectangle()
+            .frame(width: 100, height: 100)
+            .frame(maxWidth: .infinity, alignment: animate1 ? .leading : .trailing)
+            .background(.green)
+            .frame(maxHeight: .infinity, alignment: animate2 ? .top : .bottom)
+            .background(.orange)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.red)
+      }
+    }
+    .animation(.spring, value: animate1)
+    .animation(.spring, value: animate2)
+    
+    
+  }
+}
+
+#Preview {
+  AnimationUpdatedBootcamp()
+}
